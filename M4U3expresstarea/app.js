3a151b3705d3f1fa1e0624f5e39756c1e4f6e3ad
felicitas soria <soriafelicitas@hotmail.com>
2023-01-02 15:22:21 -0300

    tarea express m4u3

---
+++ M4U3expresstarea/app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');//ARMAR RUTAS ACÁ Y RETOMAR EN APP.USE
+var nosotrosRouter = require('./routes/nosotros'); //se va a nosotros.js
+var serviciosRouter = require('./routes/servicios'); //se va a servicios.js
+var contactRouter = require('./routes/contact'); //se va a contact.js
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);//ARMADO DE ROUTES
+app.use('/users', usersRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/servicios', serviciosRouter);
+app.use('/contact', contactRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
