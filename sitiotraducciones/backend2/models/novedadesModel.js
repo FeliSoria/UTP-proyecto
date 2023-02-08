90e50a04c60316f8eeecc9f88f5add2463fb8e18
felicitas soria <soriafelicitas@hotmail.com>
2023-02-08 16:29:43 -0300

    back con novedades ok

--- sitiotraducciones/backend2/models/novedadesModel.js
+++ sitiotraducciones/backend2/models/novedadesModel.js
@@ -10,7 +10,7 @@ async function getNovedades() {
 
 async function insertNovedades(obj) {
     try {
-        var query = "insert into novedades set ?";
+        var query = "insert into novedades set ? ";
         var rows = await pool.query(query, [obj]);
         return rows;
     } catch (error) {
@@ -19,5 +19,29 @@ async function insertNovedades(obj) {
     }
 }
 
+async function deleteNovedadesById(id) {
+    var query = "delete from novedades where id = ? ";
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
 
-module.exports = { getNovedades, insertNovedades }
+
+async function getNovedadesById(id) {
+    var query = "select * from novedades where id = ? ";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function modificarNovedadesById(obj, id) {
+    try{
+        var query = "update novedades set ? where id=?";
+    var rows = await pool.query(query, [obj, id]);
+    return rows;
+} catch (error){
+    throw error;
+}
+    
+}
+
+
+module.exports = { getNovedades, insertNovedades, deleteNovedadesById, getNovedadesById, modificarNovedadesById   }
--- sitiotraducciones/backend2/routes/admin/novedades.js
+++ sitiotraducciones/backend2/routes/admin/novedades.js
@@ -14,7 +14,7 @@ router.get('/', async function (req, res, next) { //listó novedades
 });
 
 router.get('/agregar', (req, res, next) => { //agregar novedades
-   
+
     res.render('admin/agregar', {
         layout: 'admin/layout',
 
@@ -22,25 +22,61 @@ router.get('/agregar', (req, res, next) => { //agregar novedades
 });
 
 router.post('/agregar', async (req, res, next) => {
-try {
-    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
-        await novedadesModel.insertNovedades(req.body);
-        res.redirect('admin/novedades')
-    } else {
-        res.render('admin/agregar', {
-            layout: 'admin/layout',
-            error: true, message: 'Todos los campos son requeridos'
-        })
-    }
+    try {
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+            await novedadesModel.insertNovedades(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true, message: 'Todos los campos son requeridos'
+            })
+        }
 
-} catch (error) {
-    console.log(error)
-    res.render ('/admin/agregar',{
-        layout: 'admin/layout',
-        error: true, message: 'No se cargó la novedad'
-    });
-}
+    } catch (error) {
+        console.log(error)
+        res.render('/admin/agregar', {
+            layout: 'admin/layout',
+            error: true, message: 'No se cargó la novedad'
+        });
+    }
 });  //agregar novedades por post form
 
+
+router.get('/eliminar/:id', async (req, res, next) => { //delete novedades
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+});
+
+
+router.get('/modificar/:id', async (req, res, next) => { //update novedades
+    let id = req.params.id;
+    let novedades = await novedadesModel.getNovedadesById(id);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedades
+    })
+});
+
+
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        let obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+        await novedadesModel.modificarNovedadesById(obj, req.body.id);
+        res.redirect('/admin/novedades')
+    } catch (error) {
+    console.log(error)
+    res.render('/admin/modificar', {
+        layout: 'admin/layout',
+        error: true, message: 'No se modificó la novedad'
+    });
+}
+});  //modificar novedadby Id
 
 module.exports = router;
